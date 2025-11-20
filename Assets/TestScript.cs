cc4f5bb459388b597f3a342b01ab1cbe44048563
OuterFrog <91077751+OuterFrog@user.noreply.github.com>
2025-11-20 17:59:25 +0100

    Project test

---
+++ Assets/TestScript.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class TestScript : MonoBehaviour
+{
+    [SerializeField] int test;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+        test++;
+        Debug.Log(test);
+    }
+}
